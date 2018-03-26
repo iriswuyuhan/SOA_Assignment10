b0c6fcc1342ec200fa7cdf20ad07d5048dd7d1e9
Thpffcj <1441732331@qq.com>
2018-03-26 17:58:41 +0800

    .

--- src/cn/edu/nju/soa/wsdl/impl/StuScoreImpl.java
+++ src/cn/edu/nju/soa/wsdl/impl/StuScoreImpl.java
@@ -1,9 +1,13 @@
 package cn.edu.nju.soa.wsdl.impl;
 
+import cn.edu.nju.soa.model.CourseScore;
 import cn.edu.nju.soa.model.CourseScoreList;
+import cn.edu.nju.soa.model.ScoreAttributeType;
+import cn.edu.nju.soa.model.ScoreType;
 import cn.edu.nju.soa.wsdl.*;
 
 import javax.xml.ws.Holder;
+import java.util.List;
 
 public class StuScoreImpl implements StuScoreInterface {
 
@@ -26,5 +30,17 @@ public class StuScoreImpl implements StuScoreInterface {
     @Override
     public void delete(Holder<CourseScoreList> parameters) throws CourseIdFault, StudentIdFault, ScoreUpdateFault, ScoreTypeFault {
 
+    }
+
+    private void conversion(CourseScoreList courseScoreList) {
+        List<CourseScore> courseScores = courseScoreList.getCourseScore();
+        courseScores.forEach(courseScore -> {
+            List<ScoreType> scoreTypes = courseScore.getScore();
+            String studentId
+            // 课程编号
+            String courseId = courseScore.getCourseId();
+            // 成绩性质
+            ScoreAttributeType scoreAttributeType = courseScore.getScoreType();
+        });
     }
 }
